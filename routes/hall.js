d5604d2d629fc585c5448ec0f9811fb8bac4b0c0
LI <li.ding@borocredit.com>
2021-02-16 16:22:27 -0600

    adding get rooms

--- routes/hall.js
+++ routes/hall.js
@@ -3,30 +3,47 @@ const Room = require("../models/room");
 
 router.post("/hall/create_room", async (req, res) => {
     if (!req.body.stake || !req.body.players) {
-      console.log(req.body);
-      res.json({ success: false, message: "Please enter stake or player" });
+        console.log(req.body);
+        res.json({ success: false, message: "Please enter stake or player" });
     } else {
-      try {
-        let newRoom = new Room();
-        newRoom.name = req.body.name;
-        newRoom.stake = req.body.stake;
-        newRoom.players = req.body.players;
-        newRoom.room_status = req.body.room_status;
-        newRoom.starting_time = Date.now();
-        await newRoom.save();
+        try {
+            let newRoom = new Room();
+            newRoom.name = req.body.name;
+            newRoom.stake = req.body.stake;
+            newRoom.players = req.body.players;
+            newRoom.room_status = req.body.room_status;
+            newRoom.starting_time = Date.now();
+            await newRoom.save();
   
-        res.json({
-          success: true,
-          message: "Successfully created a new room"
-        });
-      } catch (err) {
-        res.status(500).json({
-          success: false,
-          message: err.message
-        });
-      }
+            res.json({
+                success: true,
+                message: "Successfully created a new room"
+            });
+        } catch (err) {
+            res.status(500).json({
+                success: false,
+                message: err.message
+            });
+        }
     }
-  });
+});
+
+router.get("/hall/rooms", async (req, res) => {
+    try {
+        let rooms = await Room.find();
+
+        res.json({
+            success: true,
+            rooms: rooms,
+            message: "Successfully created a new room"
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+});
 
 
 // router.get("/auth/user", verifyToken, async (req, res) => {

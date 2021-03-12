88dd870e5eaeb02c677419b6ade3402471fdfc99
LI <li.ding@borocredit.com>
2021-03-11 21:49:59 -0600

    fox

--- models/room.js
+++ models/room.js
@@ -10,7 +10,15 @@ const RoomSchema = new Schema({
     room_status: Number,
     starting_time: Date,
     end_time: Date,
-    blind_type: String  
+    blind_type: String,
+    seat_status: [Number],
+    button_position: Number,
+    next_button_position: Number,
+    player_bank: [Number],
+    player_name: [String],
+    player_avatar: [Number],
+    table_chips: [Number],
+    remaining_time: [Number]
 });
 
 module.exports = mongoose.model('room', RoomSchema);

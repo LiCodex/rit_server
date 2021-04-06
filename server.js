cf6675948b49647da338b76f499fe83b17407b2a
LI <li.ding@borocredit.com>
2021-04-06 18:02:06 -0500

    fix

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ wss.on('connection', function connection(ws) {
     if (message == "\"hello\"") {
       console.log('123');
 
-      var cmd = JSON.stringify({"c":"index","m":"console","data":{}})
+      var cmd = JSON.stringify({"c":"index","m":"console","data":{"result": [123, "test"]}})
       ws.send(cmd);
     }
     wss.clients.forEach(function each(client) {

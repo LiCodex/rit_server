0f018d0e2572b1743839035d742e9c2140108f09
LI <li.ding@borocredit.com>
2021-04-07 21:57:15 -0500

    fix

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ wss.on('connection', function connection(ws) {
 
 
   ws.on('message', function incoming(message) {
-    console.log(message.toString());
+    console.log(JSON.stringify(message));
     console.log(typeof message);
     console.log(message == "\"hello\"");
     if (message == "\"hello\"") {

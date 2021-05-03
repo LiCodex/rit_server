56cb824da20889f221e222399dd05544da5cbba2
LI <li.ding@borocredit.com>
2021-05-02 20:21:02 -0500

    fix

--- server.js
+++ server.js
@@ -101,8 +101,8 @@ app.listen(3000, err => {
 
 
 function load_room_info() {
-  var rooms = Room.find();
-  for (var room in rooms) {
+  var rs = Room.find();
+  for (var room in rs) {
     total_rooms++;
     rooms[room.id] = {"players": room.players, "status": room.room_status, "stake": room.stake, "name": room.name, "blind_type": room.blind_type, "starting_time": room.starting_time}
 

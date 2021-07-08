ea7c72c5cc5f247c8ef8f88579ca10d3de9635c4
LI <li.ding@borocredit.com>
2021-07-08 14:30:59 -0500

    fix

--- game_server/deck.js
+++ game_server/deck.js
@@ -6,7 +6,7 @@ const Deck = class{
     next_card_index = 0;
 
     constructor() {
-        cards = [];
+        this.cards = [];
         var index = 0;
         for (var suite = card.Card.NO_OF_SUITES - 1; suite >= 0; suite--) {
             for (var rank = card.Card.NO_OF_RANKS - 1; rank >= 0; rank--) {
@@ -22,31 +22,31 @@ const Deck = class{
             card[old_index] = card[new_index];
             card[new_index] = tmp_card;
         }
-        next_card_index = 0;
+        this.next_card_index = 0;
     }
 
     reset() {
-        next_card_index = 0;
+        this.next_card_index = 0;
     }
 
     deal() {
-        if (next_card_index + 1 > NO_OF_CARDS) {
+        if (this.next_card_index + 1 > NO_OF_CARDS) {
             throw 'no cards left in the deck';
         }
-        return cards[next_card_index++];
+        return cards[this.next_card_index++];
     }
 
     deal(no_of_cards){
         if (no_of_cards < 1) {
             throw 'no_of_cards < 1';
         }
-        if (next_card_index + no_of_cards >= NO_OF_CARDS) {
+        if (this.next_card_index + no_of_cards >= NO_OF_CARDS) {
             throw 'No cards in the deck';
         }
 
         dealt_cards = [];
         for (var i = 0; i < no_of_cards; i++) {
-            dealt_cards.push(cards[next_card_index++]);
+            dealt_cards.push(cards[this.next_card_index++]);
         }
 
         return dealt_cards;

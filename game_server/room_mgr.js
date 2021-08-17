f11603b10c67054a3ba75fe82b7525d4e838e7f1
ldingdeveloper@gmail.com <ldingdeveloper@gmail.com>
2021-08-17 16:51:40 -0500

    fix players

--- game_server/room_mgr.js
+++ game_server/room_mgr.js
@@ -44,7 +44,9 @@ function check_start(room) {
 };
 
 exports.room_join = function(message) {
-    var room_id = message.room_id;
+    var room_id = message.data.key;
+    console.log("room_id");
+    console.log(room_id);
     var room = rooms.filter(room => room["_id"] == room_id)[0];
     return { success: true, players: room["players"] }
 };
